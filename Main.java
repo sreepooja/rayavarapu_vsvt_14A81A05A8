ad3f87d48e3cc7408e96b94652d15e4fdafa5f3c
sreepooja <31965971+sreepooja@users.noreply.github.com>
2017-09-16 14:16:21 +0530

    1

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+      int id;
+      String name;
+      String address;
+      System.out.println("enter student id");
+      System.out.println("enter name of student");
+      System.out.println("enter address of the student");
+      
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
